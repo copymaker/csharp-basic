b92b5ab68c33567811e970bf7df5dc859d9feb27
copymaker <duoback0@gmail.com>
2020-07-11 18:18:29 +0900

    Ex003 bool 변수 타입

--- RoadBook.CsharpBasic.Chapter02/Program.cs
+++ RoadBook.CsharpBasic.Chapter02/Program.cs
@@ -7,8 +7,11 @@
             // Examples.Ex001 ex001 = new Examples.Ex001();
             // ex001.Run();
             
-            Examples.Ex002 ex002 = new Examples.Ex002();
-            ex002.Run();
+            // Examples.Ex002 ex002 = new Examples.Ex002();
+            // ex002.Run();
+            
+            Examples.Ex003 ex003 = new Examples.Ex003();
+            ex003.Run();
         }
     }
 }
