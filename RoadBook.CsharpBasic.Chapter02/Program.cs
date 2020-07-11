5d78d1ff86171d7db4c2a2ad037066346e816595
copymaker <duoback0@gmail.com>
2020-07-11 18:06:10 +0900

    Ex001 숫자를 담는 변수 타입

---
+++ RoadBook.CsharpBasic.Chapter02/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace RoadBook.CsharpBasic.Chapter02
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Examples.Ex001 ex001 = new Examples.Ex001();
+            ex001.Run();
+        }
+    }
+}
