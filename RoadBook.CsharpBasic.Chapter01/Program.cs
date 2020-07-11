99c097571e725902409d864d7596764748643133
copymaker <duoback0@gmail.com>
2020-07-11 17:11:01 +0900

    Hello 클래스 생성
    Examples 폴더 안에 Hello 클래스를 불러들여 Run 기능을 수행

---
+++ RoadBook.CsharpBasic.Chapter01/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace RoadBook.CsharpBasic.Chapter01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Examples.Hello hello = new Examples.Hello();
+            hello.Run();
+        }
+    }
+}
