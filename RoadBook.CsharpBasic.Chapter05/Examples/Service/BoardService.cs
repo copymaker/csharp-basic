76e8484cb59d10469035fb235ee4c13c0eb9d18f
copymaker <duoback0@gmail.com>
2020-07-12 10:51:23 +0900

    Chapter05 게시판 클래스 및 서비스

---
+++ RoadBook.CsharpBasic.Chapter05/Examples/Service/BoardService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using RoadBook.CsharpBasic.Chapter05.Examples.Model;
+
+namespace RoadBook.CsharpBasic.Chapter05.Examples.Service
+{
+    public class BoardService
+    {
+        private Board board;
+
+        public BoardService()
+        {
+            board = new Board();
+        }
+
+        public BoardService(Board board)
+        {
+            this.board = board;
+        }
+
+        public void Save(int number, string title, string content, string writer)
+        {
+            board.Number = number;
+            board.Title = title;
+            board.Content = content;
+            board.Writer = writer;
+            board.CreateDate = DateTime.Now;
+            board.UpdateDate = DateTime.Now;
+            
+            Console.WriteLine("게시물이 저장되었습니다.");
+        }
+
+        public void Update(string title, string content, string writer)
+        {
+            board.Title = title;
+            board.Content = content;
+            board.Writer = writer;
+            board.UpdateDate = DateTime.Now;
+            
+            Console.WriteLine("게시물이 수정되었습니다.");
+        }
+
+        public void Delete()
+        {
+            board = null;
+            
+            Console.WriteLine("게시물이 삭제되었습니다.");
+        }
+
+        public void Read()
+        {
+            if (board != null)
+            {
+                Console.WriteLine("{0}번 게시물", board.Number);
+                Console.WriteLine("제목 : {0}", board.Title);
+                Console.WriteLine("작성일 : {0}", board.CreateDate);
+                Console.WriteLine("수정일 : {0}", board.UpdateDate);
+                Console.WriteLine("글쓴이 : {0}", board.Writer);
+                Console.WriteLine("내용 : {0}", board.Content);
+            }
+            else
+            {
+                Console.WriteLine("게시물이 없습니다.");
+            }
+        }
+    }
+}
---
+++ RoadBook.CsharpBasic.Test/Chapter05/BoardTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NUnit.Framework;
+using RoadBook.CsharpBasic.Chapter05.Examples.Model;
+using RoadBook.CsharpBasic.Chapter05.Examples.Service;
+
+namespace RoadBook.CsharpBasic.Test.Chapter05
+{
+    [TestFixture]
+    public class BoardTests
+    {
+        [Test]
+        public void RuntTest()
+        {
+            // 기본 생성자 이용
+            int number = 1;
+            string title = "첫 번째 게시글입니다.";
+            string content = "첫 번째 공지사항입니다.";
+            string writer = "운영자";
+            
+            BoardService boardService = new BoardService();
+            boardService.Save(number, title, content, writer);
+            boardService.Read();
+            
+            Console.WriteLine("=====");
+            title = "첫 번째 게시글 수정!!";
+            boardService.Update(title, content, writer);
+            boardService.Read();
+            
+            Console.WriteLine("=====");
+            boardService.Delete();
+            boardService.Read();
+            
+            Console.WriteLine("=====");
+            
+            // 생성자 오버로딩 이용
+            Board board = new Board();
+            board.Number = 2;
+            board.Title = "두 번째 게시글입니다.";
+            board.Content = "두 번째 공지사항입니다.";
+            board.Writer = "운영자";
+            board.CreateDate = DateTime.Now;
+            board.UpdateDate = DateTime.Now;
+            
+            BoardService anotherBoardService = new BoardService(board);
+            anotherBoardService.Read();
+        }
+    }
+}
