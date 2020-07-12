0562e32848c5436da6e93683131948c923469741
copymaker <duoback0@gmail.com>
2020-07-12 15:25:43 +0900

    Chapter08 디렉토리 및 파일 생성

---
+++ RoadBook.CsharpBasic.Chapter08/Examples/Ex003.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace RoadBook.CsharpBasic.Chapter08.Examples
+{
+    public class Ex003
+    {
+        private readonly string _currentDirectory = Environment.CurrentDirectory;
+
+        public void Run()
+        {
+            FileInfo fileInfo = new FileInfo(_currentDirectory + @"\data\log.txt");
+
+            Console.WriteLine($"저장경로 : {fileInfo.DirectoryName}");
+            Console.WriteLine($"파일명 : {fileInfo.Name}");
+
+            Console.WriteLine("=== 파일 내용 ===");
+            using (StreamReader sr = new StreamReader(fileInfo.FullName))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
--- RoadBook.CsharpBasic.Chapter08/Program.cs
+++ RoadBook.CsharpBasic.Chapter08/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace RoadBook.CsharpBasic.Chapter08
+﻿using RoadBook.CsharpBasic.Chapter08.Examples;
+
+namespace RoadBook.CsharpBasic.Chapter08
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Ex003 ex003 = new Ex003();
+            ex003.Run();
         }
     }
 }
